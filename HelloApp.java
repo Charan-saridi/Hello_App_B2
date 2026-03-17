941e55a3c98bf4b670d2cb918d6f67966e0c866e
sc2152-eng <sc2152@srmist.edu.in>
2026-03-17 09:28:54 +0530

    UC3: Display Hello with name or default World

--- HelloApp.java
+++ HelloApp.java
@@ -1,11 +1,14 @@
 public class HelloApp {
+
     public static void main(String[] args) {
 
-        if(args.length > 0) {
+        if (args.length > 0) {
             System.out.println("Hello " + args[0]);
         } else {
-            System.out.println("Hello User");
+            System.out.println("Hello World");
         }
 
     }
+
+
 }
