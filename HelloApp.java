6a5ca370747b3a22fca2052ed37ec0460d30475f
sc2152-eng <sc2152@srmist.edu.in>
2026-03-25 08:43:41 +0530

    uc5

--- HelloApp.java
+++ HelloApp.java
@@ -1,17 +1,12 @@
 public class HelloApp {
-
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         } else {
-
-            for (int i = 0; i < args.length; i++) {
-                System.out.println("Hello " + args[i]);
+            for (String name : args) {   // enhanced for loop
+                System.out.println("Hello, " + name + "!");
             }
-
         }
-
     }
-
 }
