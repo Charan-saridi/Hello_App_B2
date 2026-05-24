0125c3d5e7991d0cea17e6faade97f43e070e5e5
Charan-saridi <saridicharansai@gmail.com>
2026-05-24 11:43:25 +0530

    UC7: Added String.join method for greeting app\

--- HelloApp.java
+++ HelloApp.java
@@ -2,22 +2,18 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        // If no arguments
+        // Check if no arguments are provided
         if (args.length == 0) {
+
             System.out.println("Hello, World!");
-            return;
+
+        } else {
+
+            // Join all names using String.join()
+            String names = String.join(", ", args);
+
+            // Print greeting
+            System.out.println("Hello, " + names + "!");
         }
-
-        String names = "";
-
-        // Enhanced for loop
-        for (String name : args) {
-            names += name + ", ";
-        }
-
-        // Remove trailing comma and space using substring
-        names = names.substring(0, names.length() - 2);
-
-        System.out.println("Hello, " + names);
     }
 }
